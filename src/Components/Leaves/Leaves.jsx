d387d34b97eada4a7c51a9b896d6224e9cd58de5
Zeeshan Ashraf <150527429+Zeshig01@users.noreply.github.com>
2024-12-27 08:13:15 -0800

    add

--- src/Components/Leaves/Leaves.jsx
+++ src/Components/Leaves/Leaves.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Leaves() {
+  const [staffList , setStaffList]=useState([])
+  useEffect((()=>
+    {
+      const storedstaff = JSON.parse(localStorage.getItem('staffList')) || [];      
+      setStaffList(storedstaff)
+  }), [])
   return (
-    <div>Leaves</div>
+    <div>
+      <h1>Staff List</h1>
+      {
+        staffList.map((staff,i)=>(
+          <div className='p-4 my-10 shadow-md' key={i}>
+            <p>{staff.name}</p>
+           
+          </div>
+        ))
+      }
+      
+    </div>
   )
 }
 
