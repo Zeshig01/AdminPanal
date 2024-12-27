f10bccb2351c417b1c788b375de3f876059c7025
Zeeshan Ashraf <150527429+Zeshig01@users.noreply.github.com>
2024-12-27 09:33:18 -0800

    add

--- src/Components/Leaves/Leaves.jsx
+++ src/Components/Leaves/Leaves.jsx
@@ -2,16 +2,33 @@ import React, { useEffect, useState } from 'react'
 
 function Leaves() {
   const [staffList , setStaffList]=useState([])
+  const [searchTerm, setSearchTerm]=useState('')
   useEffect((()=>
     {
       const storedstaff = JSON.parse(localStorage.getItem('staffList')) || [];      
       setStaffList(storedstaff)
   }), [])
+    
+    const filteredStaff = staffList.filter(staff => staff.name.toLowerCase().includes(searchTerm.toLowerCase()))
+    
+
   return (
     <div>
-      <h1>Staff List</h1>
+      <div>
+     
+      <input
+        type="text"
+        placeholder="Search staff"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)} 
+        className="border w-full p-2 mb-4"
+      />
+      <div>
+        <button >Leave</button>
+      </div>
+      </div>
       {
-        staffList.map((staff,i)=>(
+        filteredStaff.map((staff,i)=>(
           <div className='p-4 my-10 shadow-md' key={i}>
             <p>{staff.name}</p>
            
